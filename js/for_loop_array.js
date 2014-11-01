6d72e99e3605fc65f0abe1ea57a7bca136043717
otuyelu <jotuyelu@yahoo.com>
2014-10-31 23:37:39 -0400

    Update for_loop_array.js

--- js/for_loop_array.js
+++ js/for_loop_array.js
@@ -1,7 +1,15 @@
-// Let's print out every element of an array using a for loop
+// Introduction to 'For' Loops in J  excercise 11/13
 
 var cities = ["Melbourne", "Amman", "Helsinki", "NYC","Lagos","Paris"];
 
 for (var i = 0; i < cities.length; i++) {
     console.log("I would like to visit " + cities[i]);
+}
+
+
+// Introduction to 'For' Loops in J  excercise 12/13
+
+var names = ["Mom","Ayo","Tunde","Nunu","Dad"];
+for (var i = 0; i < names.length; i ++) {
+    console.log("I know someone called " + names[i]);
 }
