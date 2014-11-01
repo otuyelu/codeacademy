1734fc5ffb76715a8be81f5586937cd4e70df691
otuyelu <jotuyelu@yahoo.com>
2014-11-01 01:05:04 -0400

    Create I_can_count.js

---
+++ js/I_can_count.js
@@ -0,0 +1,12 @@
+// Introduction to 'While' Loops in 8/11
+
+for (var i = 1; i < 5; i++) {
+    console.log("Counting to " + i);
+    count = 0;
+    while (count < i+1) {
+        console.log(count);        
+        count += 1;
+    };
+    
+}
+console.log("I count");
