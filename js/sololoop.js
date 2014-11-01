c42d6176e4521179cfae4806aa3c67e8e942c965
otuyelu <jotuyelu@yahoo.com>
2014-11-01 00:46:18 -0400

    Create sololoop.js

---
+++ js/sololoop.js
@@ -0,0 +1,13 @@
+// Introduction to 'While' Loops in J 6/11
+//Remember to make your condition true outside the loop!
+
+var soloLoop = function(){
+    //Your code goes here!
+    var loop = true;
+    while (loop) {
+        console.log( "Looped once!");
+        loop = false;
+    } 
+};
+
+soloLoop();
