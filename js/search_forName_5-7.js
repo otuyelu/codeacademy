9626f7c6fa923024a304a5d52852682edd32ad25
otuyelu <jotuyelu@yahoo.com>
2014-11-01 00:01:52 -0400

    Create search_forName_5-7.js

---
+++ js/search_forName_5-7.js
@@ -0,0 +1,13 @@
+// Search Text for Your Name 5/7
+/*jshint multistr:true */
+var text = "Jide got up today determined to get Jide coding again after Jide took so many days off";
+var myName = "Jide";
+var hits = [];
+
+for (var i = 0; i < text.length; i ++) {
+    if (myName[0]=== text[i]) {
+        for (var j = i; j < i+myName.length; j ++) {
+            hits.push(text[j]);
+        } 
+    }
+}
