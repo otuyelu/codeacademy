6aa67cd403699df14eab14c68d2f42aba478e403
otuyelu <jotuyelu@yahoo.com>
2014-11-02 18:08:38 -0500

    Create logical_operator_do_you_love.js

---
+++ js/logical_operator_do_you_love.js
@@ -0,0 +1,14 @@
+// More on Control Flow in JS 10/14
+
+var iLoveJavaScript = true;
+var iLoveLearning = true;
+
+if(iLoveJavaScript && iLoveLearning) {
+  // if iLoveJavaScript AND iLoveLearning:
+  console.log("Awesome! Let's keep learning!");
+} else if(!(iLoveJavaScript || iLoveLearning)) {
+  // if NOT iLoveJavaScript OR iLoveLearning:
+  console.log("Let's see if we can change your mind.");
+} else {
+  console.log("You only like one but not the other? We'll work on it.");
+}
