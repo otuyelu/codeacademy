c357c33bfd8b661a0e126ed278dcf5f1ae7a33d0
otuyelu <jotuyelu@yahoo.com>
2014-11-06 00:46:29 -0500

    added conditional operators to some of the cases

--- js/switch_mine.js
+++ js/switch_mine.js
@@ -1,14 +1,27 @@
-// Choose Your Own Adventure 2! 4/6
+// Choose Your Own Adventure 2! 5/6
+// added conditional operators to some of the cases
 var user = prompt("Who do you think you are?").toUpperCase();
 switch(user){
     case 'CODER':
-        console.log("Well then, get those codes lined up");
+        var rtfm = prompt("Did you read the application documentations?").toUpperCase();
+        var forked = prompt("Have you created a local copy, to work from?").toUpperCase();
+        if (rtfm === 'YES' || forked === 'YES'){
+            console.log("Do you have anything to add to the code?");
+        }else{
+            console.log("You need to get familiar with the content and features in the code");
+        }
     break;
     case 'GAMER':
         console.log("So you are here to beat the record");
     break;
     case 'TESTER':
-        console.log("If find it is broken, log your observation");
+        var buggy = prompt("Did you find a bug in this application yet?").toUpperCase();
+        var logged = prompt("Have you logged any bug?").toUpperCase();
+        if (buggy === 'YES' && logged === 'YES'){
+            console.log("I knew you were a skilled Quality Assurance professional");
+        }else{
+            console.log("If you find it is broken, log your observation");
+        }
     break;
     default:
         console.log("Why are you here?");
