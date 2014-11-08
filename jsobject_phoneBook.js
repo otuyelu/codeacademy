8138fd370d0582da65f1f940778a8720d1d617d9
otuyelu <jotuyelu@yahoo.com>
2014-11-07 21:28:08 -0500

    Create jsobject_phoneBook.js

---
+++ jsobject_phoneBook.js
@@ -0,0 +1,10 @@
+// Arrays and Objects in JS 10/17
+var phonebookEntry = {};
+
+phonebookEntry.name = 'Oxnard Montalvo';
+phonebookEntry.number = '(555) 555-5555';
+phonebookEntry.phone = function() {
+  console.log('Calling ' + this.name + ' at ' + this.number + '...');
+};
+
+phonebookEntry.phone();
