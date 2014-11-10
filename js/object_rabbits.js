f7b04bc670b854b491e17785807fca567f3afa4b
otuyelu <jotuyelu@yahoo.com>
2014-11-09 21:19:42 -0500

    Create object_rabbits.js

---
+++ js/object_rabbits.js
@@ -0,0 +1,17 @@
+// Introduction to Objects I 25/33
+function Rabbit(adjective) {
+    this.adjective = adjective;
+    this.describeMyself = function() {
+        console.log("I am a " + this.adjective + " rabbit");
+    };
+}
+
+// now we can easily make all of our rabbits
+
+var rabbit1 = new Rabbit("fluffy");
+var rabbit2 = new Rabbit("happy");
+var rabbit3 = new Rabbit("sleepy");
+
+rabbit1.describeMyself();
+rabbit2.describeMyself();
+rabbit3.describeMyself();
