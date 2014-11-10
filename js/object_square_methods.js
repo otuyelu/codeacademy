2cc4658fde7d91510ae36f4c2417b347c72d8cba
otuyelu <jotuyelu@yahoo.com>
2014-11-09 20:53:19 -0500

    Create object_square_methods.js

---
+++ js/object_square_methods.js
@@ -0,0 +1,13 @@
+//Introduction to Objects I 19/33
+var square = new Object();
+square.sideLength = 6;
+square.calcPerimeter = function() {
+  return this.sideLength * 4;
+};
+// help us define an area method here
+square.calcArea = function() {
+  return this.sideLength * this.sideLength;
+};
+
+var p = square.calcPerimeter();
+var a = square.calcArea();
