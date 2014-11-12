b4ae77430cc474a3d7dba67a306d0b89ea774250
otuyelu <jotuyelu@yahoo.com>
2014-11-12 13:58:29 -0500

    Modified the "scan" method to process "quantity"

--- js/cashRegister.js
+++ js/cashRegister.js
@@ -1,38 +1,29 @@
-// Building a Cash Register 2/7
+// Building a Cash Register 4/7
 var cashRegister = {
-    total: 0,
-//insert the add method here    
+    total:0,
     add: function(itemCost){
         this.total += itemCost;
     },
-    
-    scan: function (item) {
-        switch (item) { 
-        case "eggs": 
-            this.add(0.98); 
-            break;
-        
-        case "milk": 
-            this.add(1.23); 
-            break;
-        
-        //Add other 2 items here
-        case "magazine":
-            this.add(4.99);
-            break;
-            
-        case "chocolate":
-            this.add(0.45);
-            break;
+    scan: function(item,quantity) {
+        if (typeof(quantity) === "number") {
+            this.quantity = quantity;
+        }else{
+            this.quantity = 1;
         }
-        return true;
+        switch (item) {
+        case "eggs": this.add(0.98 * this.quantity); break;
+        case "milk": this.add(1.23 * this.quantity); break;
+        case "magazine": this.add(4.99 * this.quantity); break;
+        case "chocolate": this.add(0.45 * this.quantity); break;
+        }
     }
 };
 
-//Scan 2 eggs and 3 magazines
-cashRegister.scan('eggs');
-cashRegister.scan('eggs');
-cashRegister.scan("magazine");
-cashRegister.scan("magazine");
-cashRegister.scan("magazine");
+// scan each item 4 times
+cashRegister.scan("eggs", 4);
+cashRegister.scan("milk", 4);
+cashRegister.scan("magazine", 4);
+cashRegister.scan("chocolate", 4);
+
+//Show the total bill
 console.log('Your bill is '+cashRegister.total);
