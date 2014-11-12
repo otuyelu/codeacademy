1a6644e0e60ed6920d7d538279d5cb78dfc7942d
otuyelu <jotuyelu@yahoo.com>
2014-11-12 07:09:10 -0500

    Create object_check.js

---
+++ js/object_check.js
@@ -0,0 +1,8 @@
+// Introduction to Objects II 29/30
+// what is this "Object.prototype" anyway...?
+var prototypeType = typeof Object.prototype;
+console.log(prototypeType);
+
+// now let's examine it!
+var hasOwn = Object.prototype.hasOwnProperty('hasOwnProperty');
+console.log(hasOwn);
