345d67bf26bcab402bcb5699fa6f60724cbb35c9
otuyelu <jotuyelu@yahoo.com>
2014-11-11 23:15:17 -0500

    Create object_inheritance_penguin.js

---
+++ js/object_inheritance_penguin.js
@@ -0,0 +1,21 @@
+// Introduction to Objects II 19/30
+// the original Animal class and sayName method
+function Animal(name, numLegs) {
+    this.name = name;
+    this.numLegs = numLegs;
+}
+Animal.prototype.sayName = function() {
+    console.log("Hi my name is " + this.name);
+};
+
+// define a Penguin class
+function Penguin(name){
+    this.name = name;
+    this.numLegs = 2;
+    
+};
+
+// set its prototype to be a new instance of Animal
+Penguin.prototype = new Animal();
+var penguin = new Penguin("zebra");
+penguin.sayName();
