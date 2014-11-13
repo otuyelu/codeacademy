92be9f9512b2732c4083d2e71daa89f83b91ee8e
otuyelu <jotuyelu@yahoo.com>
2014-11-13 02:17:49 -0500

    Create Introduction_to_jQuery.js

---
+++ jquery/Introduction_to_jQuery.js
@@ -0,0 +1,28 @@
+--------------------script.js------------------------
+$(document).ready(function() {
+    $('div').mouseenter(function() {
+        $('div').hide();
+    });
+});
+----------------index.html-------------------------------
+<html>
+	<head>
+		<title>Button Magic</title>
+		<script type="text/javascript" src="script.js"></script>
+        <link rel='stylesheet' type='text/css' href='stylesheet.css'/>
+	</head>
+	<body>
+     <div><br/><strong>Click Me!</strong></div>   
+	</body>
+</html>
+--------------------stylesheet.css------------------------------
+div {
+    height: 60px;
+    width: 100px;
+    border-radius: 5px;
+    background-color: #69D2E7;
+    text-align: center;
+    color: #FFFFFF;
+    font-family: Verdana, Arial, Sans-Serif;
+    opacity: 0.5;
+}
